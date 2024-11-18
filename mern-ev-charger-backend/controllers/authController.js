5aae1cb4e89979378b33bb3661c1dc8836e10c32
Goud <163510835+Tejaaa0312@users.noreply.github.com>
2024-11-17 18:37:27 -0600

    Update authController in backend
    Update authController in backend

--- mern-ev-charger-backend/controllers/authController.js
+++ mern-ev-charger-backend/controllers/authController.js
@@ -7,7 +7,7 @@ exports.register = async (req, res) => {
     try {
         const hashedPassword = await bcrypt.hash(password, 10);
         const newUser = await User.create({ email, password: hashedPassword });
-        res.status(201).json({ message: "User registered successfully." });
+        res.status(201).json({ message: "User registered successfully" });
     } catch (error) {
         res.status(500).json({ error: "User registration failed" });
     }
